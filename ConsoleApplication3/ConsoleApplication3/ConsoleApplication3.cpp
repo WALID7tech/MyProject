112b59a2ec9c006286af18a85a7c12336e0fb9bd
KhaledTech <rtx1bilion@gmail.com>
2026-01-06 23:29:22 +0100

    task 3
    my try in 2026/01/06

---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,40 @@
+﻿
+
+#include <iostream>
+#include <string>
+using namespace std;
+int Readnumber()
+{
+    int number;
+    cout << "Please enter number :" << endl;
+    cin >> number;
+    return number;
+ }
+int SumoAllDivisors(int number)
+{
+    int sum = 0;
+
+    for (int i=1; i < number; i++)
+    {
+        if (number % i == 0)
+        {
+            sum = sum + i;   
+        }
+
+    } 
+    if (sum == number)
+    {
+        cout << number << "\n"<< "it is perfect number .";
+    }
+    else
+        cout << number<< "\n" << "it is not perfect number";
+    return number;
+}
+
+
+int main()
+{
+    SumoAllDivisors(Readnumber());
+
+}
+
