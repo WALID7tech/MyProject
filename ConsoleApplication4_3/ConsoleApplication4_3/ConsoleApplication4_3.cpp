6ae3cbda17bf33f383e37b595b9ca009f94d5993
KhaledTech <rtx1bilion@gmail.com>
2026-01-17 23:13:19 +0100

    task4_3

---
+++ ConsoleApplication4_3/ConsoleApplication4_3/ConsoleApplication4_3.cpp
@@ -0,0 +1,62 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+long long readnumber(string message)
+{
+    long long number = 0;
+    do
+    {
+        cout << message << endl;
+        cin >> number;
+
+    } while (number < 0);
+    return number;
+}
+bool isPerfectNumber(long long number)
+{
+    long long sum = 0;
+
+    for (long long i = 1; i < number; i++)
+    {
+        if (number % i == 0)
+        {
+            sum += i;
+        }
+    }
+    return number == sum;
+}
+void Printnumber(long long number)
+{
+    if (isPerfectNumber(number))
+    {
+        cout << number << "\n" << "it is perfect number .";
+
+    }
+    else
+        cout << number << "\n" << "it is not perfect number" << "\n";
+
+}
+void printperfectnumberfrom1toN(long long number)
+{
+
+    for (long long s = 1; s <= number; s++)
+    {
+
+
+        if (isPerfectNumber(s))
+        {
+            cout << s << endl;
+        }
+
+    }
+
+}
+int main()
+{
+    printperfectnumberfrom1toN(readnumber("Please enter a positive number"));
+    
+    
+}
